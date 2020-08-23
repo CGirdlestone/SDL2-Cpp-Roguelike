4b2ac683b55d06e3c98a23cbcbcfa61f0daccaed
craig girdlestone <craiggirdlestone1@gmail.com>
2020-08-23 22:44:58 +0100

    Fixed a bug in rendering of stairs.

--- src/EntityFactory.cpp
+++ src/EntityFactory.cpp
@@ -661,7 +661,7 @@ void EntityFactory::makeEntity(int level, EntityType type, GameObject* entity, i
 
 void EntityFactory::makeStairs(GameObject* entity, int x, int y)
 {
-	std::string line = "< 0xef 0xd8 0xa1 3 7 17";
+	std::string line = "< 0xef 0xd8 0xa1 7 3 17";
 	makePositionComponent(entity, x, y);
 	makeRenderableComponent(line, entity);
 	Stairs *stairs = new Stairs();
